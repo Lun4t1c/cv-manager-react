929693dd6d5f622525f7cbcd09e3d456b36b658c
-Lunat1c- <arturgrabowski100@onet.pl>
2025-05-07 21:19:28 +0200

    Fixed SplitPane height

--- src/pages/CVCreatorPage/CVCreatorPage.tsx
+++ src/pages/CVCreatorPage/CVCreatorPage.tsx
@@ -6,11 +6,9 @@ import { SplitPane } from '@rexxars/react-split-pane';
 function CVCreatorPage() {
   //https://www.npmjs.com/package/@rexxars/react-split-pane
   const cssProps: CSSProperties = {
-    display: 'flex',
-    height: '100%',
-    backgroundColor: 'purple',
-    boxSizing: 'border-box'
+    height: '88%',
   }
+  
   return <>
     <SplitPane split="vertical" minSize={150} maxSize={-150} defaultSize={500} style={cssProps}>
       <form className='form-container'>
