83ba316ea669f8f33b4520b9330830630d28baf3
-Lunat1c- <arturgrabowski100@onet.pl>
2025-05-07 22:03:01 +0200

    Simple locale switching

--- src/shared/Navbar/Navbar.tsx
+++ src/shared/Navbar/Navbar.tsx
@@ -1,12 +1,21 @@
 import { useNavigate } from 'react-router-dom';
 import './Navbar.css';
+import { useTranslation } from 'react-i18next';
 
 function Navbar() {
   const navigate = useNavigate();
+  const { i18n } = useTranslation();
 
   return (
     <nav className="navbar">
       <span>CV Manager</span>
+
+      <button onClick={() => i18n.changeLanguage('pl')}>
+        Polish
+      </button>
+      <button onClick={() => i18n.changeLanguage('en')}>
+        English
+      </button>
 
       <button className="btn create-new-cv-button" onClick={() => navigate('/creator')}>
         Create new CV
