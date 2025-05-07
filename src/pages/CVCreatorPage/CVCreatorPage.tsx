71437337c0a55700c8e6cf83e383d87e36cf723a
-Lunat1c- <arturgrabowski100@onet.pl>
2025-05-07 21:04:34 +0200

    Added SplitPane

--- src/pages/CVCreatorPage/CVCreatorPage.tsx
+++ src/pages/CVCreatorPage/CVCreatorPage.tsx
@@ -1,7 +1,27 @@
+import type { CSSProperties } from 'react';
 import './CVCreatorPage.css';
+import './SplitPane.css';
+import { SplitPane } from '@rexxars/react-split-pane';
 
 function CVCreatorPage() {
-  return <>CV Creator Page</>;
+  //https://www.npmjs.com/package/@rexxars/react-split-pane
+  const cssProps: CSSProperties = {
+    display: 'flex',
+    height: '100%',
+    backgroundColor: 'purple',
+    boxSizing: 'border-box'
+  }
+  return <>
+    <SplitPane split="vertical" minSize={150} maxSize={-150} defaultSize={500} style={cssProps}>
+      <form className='form-container'>
+        <input></input>
+      </form>
+
+      <div className='preview-container'>
+        preview
+      </div>
+    </SplitPane>
+  </>;
 }
 
 export default CVCreatorPage;
