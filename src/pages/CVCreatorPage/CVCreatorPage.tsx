2b6a3f9d3063d194c96cef21d5cf6fdef1ab423c
-Lunat1c- <arturgrabowski100@onet.pl>
2025-05-07 21:44:23 +0200

    Simple form inputs

--- src/pages/CVCreatorPage/CVCreatorPage.tsx
+++ src/pages/CVCreatorPage/CVCreatorPage.tsx
@@ -6,20 +6,37 @@ import { SplitPane } from '@rexxars/react-split-pane';
 function CVCreatorPage() {
   //https://www.npmjs.com/package/@rexxars/react-split-pane
   const cssProps: CSSProperties = {
-    height: '88%',
-  }
-  
-  return <>
-    <SplitPane split="vertical" minSize={150} maxSize={-150} defaultSize={500} style={cssProps}>
-      <form className='form-container'>
-        <input></input>
-      </form>
+    height: '88%'
+  };
 
-      <div className='preview-container'>
-        preview
-      </div>
-    </SplitPane>
-  </>;
+  return (
+    <>
+      <SplitPane split="vertical" minSize={150} maxSize={-150} defaultSize={500} style={cssProps}>
+        <form className="form-container">
+          <div className="form-group">
+            <input type="email" id="email" name="email" placeholder="E-mail address" required />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              placeholder="Phone number"
+              pattern="[0-9\-]+"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <input type="url" id="url" name="url" placeholder="LinkedIn profile" required />
+          </div>
+        </form>
+
+        <div className="preview-container">preview</div>
+      </SplitPane>
+    </>
+  );
 }
 
 export default CVCreatorPage;
