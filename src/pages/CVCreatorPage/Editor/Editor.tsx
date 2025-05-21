d375a3923ed0e659bf6ed3566bc9287ed37c369f
-Lunat1c- <arturgrabowski100@onet.pl>
2025-05-21 18:15:29 +0200

    Updating and displaying skills

--- src/pages/CVCreatorPage/Editor/Editor.tsx
+++ src/pages/CVCreatorPage/Editor/Editor.tsx
@@ -3,6 +3,7 @@ import './Editor.css';
 import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "../cvStore";
 import { updateLinks, updatePersonalInfo, updateSkills } from "../cvSlice";
+import React from "react";
 
 function Editor() {
     const personalInfo = useSelector((state: RootState) => state.cv.personalInfo)
@@ -19,7 +20,46 @@ function Editor() {
         }))
     };
 
-    
+    const handleSkillChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        const { name, value } = e.target;
+
+        if (name.includes("SkillName")) {
+            dispatch(updateSkills(
+                skills.map((skill, i) =>
+                    i === parseInt(name.replace('SkillName', '')) ? { ...skill, name: value } : skill
+                )
+            ));
+        } else if (name.includes("SkillLevel")) {
+            dispatch(updateSkills(
+                skills.map((skill, i) =>
+                    i === parseInt(name.replace('SkillLevel', '')) ? { ...skill, level: value } : skill
+                )
+            ));
+        }
+    }
+
+    const addEntry = (entryType: 'Skill' | 'WorkExperience' | 'Education' | 'Link'): void => {
+        switch (entryType) {
+            case "Skill":
+                dispatch(updateSkills([
+                    ...skills,
+                    {
+                        name: "",
+                        level: ""
+                    }
+                ]));
+                break;
+
+            case "WorkExperience":
+                break;
+
+            case "Education":
+                break;
+
+            case "Link":
+                break;
+        }
+    }
 
     return (<>
         <form className="form-container">
@@ -46,7 +86,30 @@ function Editor() {
                 />
             </div>
 
-            
+            <div>
+                <div className="separator"></div>
+                {skills.map((skill: SkillModel, index) => {
+                    return (<React.Fragment key={index}>
+                        <input
+                            type="text"
+                            name={'SkillName' + index.toString()}
+                            placeholder="Skill name"
+                            value={skill.name}
+                            onChange={handleSkillChange} />
+
+                        <input
+                            type="number"
+                            min={1}
+                            max={10}
+                            name={'SkillLevel' + index.toString()}
+                            placeholder="Skill level"
+                            value={skill.level}
+                            onChange={handleSkillChange} />
+                    </React.Fragment>);
+                })}
+
+                <button type="button" onClick={() => addEntry('Skill')}>Add skill</button>
+            </div>
         </form>
     </>);
 }
--- src/pages/CVCreatorPage/Preview/Preview.tsx
+++ src/pages/CVCreatorPage/Preview/Preview.tsx
@@ -1,13 +1,21 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "../cvStore";
+import React from "react";
 
 function Preview() {
-    const cv = useSelector((state: RootState) => state.cv)
+    const cv = useSelector((state: RootState) => state.cv);
 
     return (<>
         <div className="preview-container">
             <div> {cv.personalInfo.email} </div>
             <div> {cv.personalInfo.phoneNumber} </div>
+
+            {cv.skills.map((skill, i) => {
+            return <React.Fragment key={i}>
+                <div>{skill.name} - {skill.level}</div>
+            </React.Fragment>
+        }
+        )}
         </div>
     </>);
 }
